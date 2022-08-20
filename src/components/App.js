227f1a56fea4a8f0cb653c5d17523bf818af5bc4
Alla Borisova <alla.borisova27@gmail.com>
2022-08-20 14:53:56 +0500

    refactor: modals rendering in functions

--- src/components/App.js
+++ src/components/App.js
@@ -12,13 +12,90 @@ import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 
 
-
-
-function App () {
+function Login() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="outline-primary" onClick={handleShow}>Login</Button>{' '}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Agree" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+function SignUp() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>Sign-up</Button>{' '}
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sign-up</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Agree" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Sign-up
+          </Button>
+        </Form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+function App () {
   return (
     <header>
       <Container>
@@ -32,46 +109,27 @@ function App () {
               <Nav.Link href="/">Главная</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link href="№" eventKey="textbook">Учебник</Nav.Link>
+              <Nav.Link href="#" eventKey="textbook">Учебник</Nav.Link>
             </Nav.Item>
-            
+            <Nav.Item>
+              <Nav.Link href="#" eventKey="textbook">Аудиовызов</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#" eventKey="textbook">Спринт</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="#" eventKey="textbook">Статистика</Nav.Link>
+            </Nav.Item>
           </Nav>
           </Col>
           <Col>
-            <Button variant="outline-primary" onClick={handleShow}>Login</Button>{' '}
-            <Button variant="primary">Sign-up</Button>{' '}
+            <Login />
+            <SignUp />
           </Col>
         </Row>
       </Container>
       
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Login</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Agree" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
-        </Form>
-        </Modal.Body>
-        
-      </Modal>
+      
     </header>
   );
 }
