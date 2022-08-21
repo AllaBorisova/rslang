7914878782caa68d6b75e68e8c2e257e78c7e908
Alla Borisova <alla.borisova27@gmail.com>
2022-08-21 09:22:42 +0500

    feat: homepage components in separate files

--- src/components/App.js
+++ src/components/App.js
@@ -1,137 +1,21 @@
-import React, { useState } from "react"
-import 'bootstrap/scss/bootstrap.scss';
-import "../styles/App.scss"
+import React, { useState } from "react";
+import "bootstrap/scss/bootstrap.scss";
+import "../styles/App.scss";
 
+import Header from "./header";
+import Capabilities from "./capabilities";
+import Team from "./team";
+import Footer from "./footer";
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Nav from 'react-bootstrap/Nav';
-import Form from 'react-bootstrap/Form';
-import Modal from 'react-bootstrap/Modal';
-
-
-function Login() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+function App() {
   return (
-    <>
-      <Button variant="outline-primary" onClick={handleShow}>Login</Button>{' '}
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Login</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Agree" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Login
-          </Button>
-        </Form>
-        </Modal.Body>
-      </Modal>
-    </>
+    <main>
+      <Header />
+      <Capabilities />
+      <Team />
+      <Footer />
+    </main>
   );
 }
 
-function SignUp() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
-  return (
-    <>
-      <Button variant="primary" onClick={handleShow}>Sign-up</Button>{' '}
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Sign-up</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicName">
-              <Form.Label>Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter name" />
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            
-          <Form.Group className="mb-3" controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Agree" />
-          </Form.Group>
-          <Button variant="primary" type="submit">
-            Sign-up
-          </Button>
-        </Form>
-        </Modal.Body>
-      </Modal>
-    </>
-  );
-}
-
-function App () {
-  return (
-    <header>
-      <Container>
-        <Row>
-          <Col>
-            <h1>RSLANG</h1>
-          </Col>
-          <Col xs={6}>
-          <Nav defaultActiveKey="/">
-            <Nav.Item>
-              <Nav.Link href="/">Главная</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#" eventKey="textbook">Учебник</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#" eventKey="textbook">Аудиовызов</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#" eventKey="textbook">Спринт</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="#" eventKey="textbook">Статистика</Nav.Link>
-            </Nav.Item>
-          </Nav>
-          </Col>
-          <Col>
-            <Login />
-            <SignUp />
-          </Col>
-        </Row>
-      </Container>
-      
-      
-    </header>
-  );
-}
-
-export default App
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,141 @@
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import Modal from "react-bootstrap/Modal";
+
+
+function Login() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="outline-primary" onClick={handleShow}>
+          Login
+        </Button>{" "}
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+  
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+  
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Agree" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Login
+              </Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </>
+    );
+  }
+  
+  function SignUp() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Sign-up
+        </Button>{" "}
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Sign-up</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter name" />
+              </Form.Group>
+  
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+  
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+  
+              <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Agree" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Sign-up
+              </Button>
+            </Form>
+          </Modal.Body>
+        </Modal>
+      </>
+    );
+}
+  
+function Header () {
+    return (
+      <header className="my-2">
+        <Container>
+          <Row>
+            <Col>
+              <h1>RSLANG</h1>
+            </Col>
+            <Col md={6}>
+              <Nav defaultActiveKey="/">
+                <Nav.Item>
+                  <Nav.Link href="/">Главная</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#" eventKey="textbook">
+                    Учебник
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#" eventKey="audiocall">
+                    Аудиовызов
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#" eventKey="sprint">
+                    Спринт
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="#" eventKey="statistics">
+                    Статистика
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Col>
+            <Col>
+              <Login />
+              <SignUp />
+            </Col>
+          </Row>
+        </Container>
+      </header>
+    );
+}
+  
+export default Header;
