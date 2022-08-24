18bdcdb4536df68c34ea20a2cb6fadb65831ad20
alexsmirnova13 <alexandrasmirnovae@gmail.com>
2022-08-25 00:32:09 +0300

    feat: base sprint

---
+++ src/components/LevelChoice.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Group from './Book/Group'
+
+function LevelChoice() {
+    return (
+        <div>
+            <div>X</div>
+            <div>тут текст про каждую игру</div>
+            <p>выберите сложность</p>
+            <Group />
+        </div>
+    )
+}
+
+export default LevelChoice
--- src/pages/Sprint.js
+++ src/pages/Sprint.js
@@ -1,6 +1,38 @@
 import React from 'react'
+import './Sprint.scss'
 
 function Sprint() {
-    return <h1>Sprint</h1>
+    return (
+        <div className="sprint">
+            <div className="sprint__xMark">&times;</div>
+            <div className="sprint__game">
+                <div className="sprint__timeScore">
+                    <div>20</div>
+                    <div>620</div>
+                </div>
+                <div className="sprint__interface">
+                    <div className="sprint__correctAnswer">
+                        <p>1</p>
+                        <p>2</p>
+                        <p>3</p>
+                    </div>
+                    <div className="sprint__combo">
+                        <p>I</p>
+                        <p>II</p>
+                        <p>III</p>
+                    </div>
+                    <div className="sprint__words">
+                        <audio>озвучка слов</audio>
+                        <p>слово</p>
+                        <p>предполагаемый перевод</p>
+                    </div>
+                    <div className="sprint__buttons">
+                        <button>верно &#8594; </button>
+                        <button>&#8592; не верно</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 export default Sprint
