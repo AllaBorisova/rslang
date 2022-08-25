2e47f4256f4a57dc4e9fee2052b017d59806b815
Alla Borisova <alla.borisova27@gmail.com>
2022-08-25 20:38:50 +0500

    feat: trye to add statistic

--- src/components/Auth/ApiUser.js
+++ src/components/Auth/ApiUser.js
@@ -18,18 +18,55 @@ export async function signUpUser(credentials) {
         },
         body: JSON.stringify(credentials),
     })
-    const content = await rawResponse.json();
-    console.log( content );
+    const content = await rawResponse.json()
     return await loginUser(credentials)
 }
 
-// async function getUser(userId) {
-//     const rawResponse = await fetch(`https://teamwork-rs.herokuapp.com/users${userId}`, {
-//         method: 'GET',
-//     })
-//     const content = await rawResponse.json()
-//     return content
-// }
+export async function getUser () {
+    const userDataString = localStorage.getItem( 'userData' )
+    const userData = JSON.parse(userDataString)
+    const rawResponse = await fetch(`https://teamwork-rs.herokuapp.com/users/${userData?.userId}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${userData?.token}`,
+            'Content-Type': 'application/json',
+        },
+    } )
+    
+    const content = await rawResponse.json()
+    console.log(content);
+    return content
+}
+
+export async function updateUser ( credentials ) {
+    const userDataString = localStorage.getItem( 'userData' )
+    const userData = JSON.parse(userDataString)
+    const rawResponse = await fetch(`https://teamwork-rs.herokuapp.com/users/${userData?.userId}`, {
+        method: 'PUT',
+        headers: {
+            'Authorization': `Bearer ${userData?.token}`,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
+    })
+    const content = await rawResponse.json()
+    return content
+}
+
+export async function getUserStatistic ( ) {
+    const userDataString = localStorage.getItem( 'userData' )
+    const userData = JSON.parse(userDataString)
+    const rawResponse = await fetch(`https://teamwork-rs.herokuapp.com/users/${userData?.userId}/statistics`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${userData?.token}`,
+            'Content-Type': 'application/json',
+        }
+    } )
+    
+    const content = await rawResponse.json()
+    return content
+}
 
 // function ShowNameUser (props) {
 //     const name = await getUser( props.userId );
