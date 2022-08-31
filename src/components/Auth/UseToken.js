c786cc6b6e5ec2a891480f81898a55939ffa00f0
Alla Borisova <alla.borisova27@gmail.com>
2022-08-31 12:37:23 +0500

    refactor: clear file  comments

--- src/components/Auth/UseToken.js
+++ src/components/Auth/UseToken.js
@@ -1,37 +1,21 @@
 import { useState } from 'react'
 
-
-
 export default function useToken () {
     const getToken = () => {
         const userDataString = localStorage.getItem('userData')
         const userData = JSON.parse( userDataString )
-        // setIsAuth(true)
-        return userData?.token
+        return userData?.token ? userData?.token : null
     }
 
-    // const [isAuth, setIsAuth] = useState(false)
     const [ token, setToken ] = useState( getToken() )
-    
    
-
-    // const getUserId = () => {
-    //     const userDataString = localStorage.getItem('userData')
-    //     const userData = JSON.parse(userDataString)
-    //     return userData?.userId
-    // }
-
     const saveUserData = (userToken) => {
         localStorage.setItem('userData', JSON.stringify(userToken))
-        // setIsAuth(true)
         setToken(userToken.token)
     }
     const logout = () => {
-        // setIsAuth(false)
-
         localStorage.removeItem('userData')
         setToken(null)
-        // setIsAuth(false)
     }
 
     return {
