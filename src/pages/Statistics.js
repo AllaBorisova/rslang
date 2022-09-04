18f8789f438e95e4524b6240292abdbc33afbe8c
Alla Borisova <alla.borisova27@gmail.com>
2022-09-04 23:20:04 +0500

    feat: add game from textbook and statistic

--- src/pages/Statistics.js
+++ src/pages/Statistics.js
@@ -1,48 +1,186 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import useToken from '../components/Auth/UseToken';
 import Container from 'react-bootstrap/Container';
+import Loading from '../components/Loading';
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
 
 function Statistics() {
     const { token, setToken, logout, userId } = useToken();
     const [userStatistic, setUserStatistic] = useState([]);
-    if (token) {
-        useEffect(() => {
-            const getStatistic = async () => {
-                const rawResponse = await fetch(`https://teamwork-rs.herokuapp.com/users/${userId}/statistics`, {
+    const [userStatisticHard, setUserStatisticHard] = useState([]);
+    const [ userStatisticEasy, setUserStatisticEasy ] = useState( [] );
+    const [userStatisticSprint, setUserStatisticSprint] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    const getUserAggregatedWords = async (userId, token) => {
+        try {
+            setError('');
+            setLoading(true);
+            const rawResponse = await fetch(
+                `https://teamwork-rs.herokuapp.com/users/${userId}/aggregatedWords?wordsPerPage=1000`,
+                {
                     method: 'GET',
+                    withCredentials: true,
                     headers: {
                         Authorization: `Bearer ${token}`,
                         Accept: 'application/json',
-                    }
-                } );
-                console.log(content)
-                const content = await rawResponse.json();
-                
-                // try {
-                //     const rawResponse = await fetch(`https://teamwork-rs.herokuapp.com/users/${userId}/statistics`, {
-                //         method: 'GET',
-                //         headers: {
-                //             Authorization: `Bearer ${token}`,
-                //             Accept: 'application/json',
-                //         }
-                //     } );
-                    
-                //     const content = await rawResponse.json();
-                //     console.log(res);
-                //     setUserStatistic(res.data);
-                // } catch (error) {
-                //     console.log(error);
-                // }
-            };
-            getStatistic();
+                    },
+                }
+            );
+            const content = await rawResponse.json();
+            console.log('getaaaa', content);
+            const res = content[0].paginatedResults;
+            setUserStatistic(res);
+            setLoading(false);
+        } catch (e) {
+            const error = e;
+            setLoading(false);
+            setError(error.message);
+        }
+    };
+
+    const getUserAggregatedWordsHard = async (userId, token) => {
+        try {
+            setError('');
+            setLoading(true);
+            const rawResponse = await fetch(
+                `https://teamwork-rs.herokuapp.com/users/${userId}/aggregatedWords?wordsPerPage=3600&filter={"userWord.difficulty":"hard"}`,
+                {
+                    method: 'GET',
+                    withCredentials: true,
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        Accept: 'application/json',
+                    },
+                }
+            );
+            const content = await rawResponse.json();
+            console.log('geta', content);
+            const res = content[0].paginatedResults;
+            setUserStatisticHard(res);
+            setLoading(false);
+        } catch (e) {
+            const error = e;
+            setLoading(false);
+            setError(error.message);
+        }
+    };
+
+    const getUserAggregatedWordsEasy = async (userId, token) => {
+        try {
+            setError('');
+            setLoading(true);
+            const rawResponse = await fetch(
+                `https://teamwork-rs.herokuapp.com/users/${userId}/aggregatedWords?wordsPerPage=3600&filter={"userWord.difficulty":"easy"}`,
+                {
+                    method: 'GET',
+                    withCredentials: true,
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        Accept: 'application/json',
+                    },
+                }
+            );
+            const content = await rawResponse.json();
+            console.log('geta', content);
+            const res = content[0].paginatedResults;
+            setUserStatisticEasy(res);
+            setLoading(false);
+        } catch (e) {
+            const error = e;
+            setLoading(false);
+            setError(error.message);
+        }
+    };
+
+    const getUserAggregatedWordsSprint = async (userId, token) => {
+        try {
+            setError('');
+            setLoading(true);
+            const rawResponse = await fetch(
+                `https://teamwork-rs.herokuapp.com/users/${userId}/aggregatedWords?wordsPerPage=100&filter={"userWord.optional.source":"game"}`,
+                {
+                    method: 'GET',
+                    withCredentials: true,
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        Accept: 'application/json',
+                    },
+                }
+            );
+            const content = await rawResponse.json();
+            console.log('getsp', content);
+            const res = content[0].paginatedResults;
+            setUserStatisticEasy(res);
+            setLoading(false);
+        } catch (e) {
+            const error = e;
+            setLoading(false);
+            setError(error.message);
+        }
+    };
+
+    if (token) {
+        useEffect(() => {
+            getUserAggregatedWords(userId, token);
+            getUserAggregatedWordsHard(userId, token);
+            getUserAggregatedWordsEasy( userId, token );
+            getUserAggregatedWordsSprint( userId, token );
         }, []);
     }
+    
+    
+    if (loading) {
+        return (
+            <div>
+                <div>
+                    <Row className="justify-content-md-center">
+                        <Loading />
+                    </Row>
+                </div>
+            </div>
+        );
+    }
 
+    if (error) {
+        return (
+            <div>
+                <div>
+                    <Row className="justify-content-md-center">
+                        <p>{error}</p>
+                    </Row>
+                </div>
+            </div>
+        );
+    }
     return (
         <section className="py-4 full-section">
             <Container>
-                {userStatistic && <h1>Статистика недоступна</h1>}
+                {userStatistic && (
+                    <>
+                        <h1>Статистика доступна</h1>
+                        <h2>Сложные слова {userStatisticHard.length}</h2>
+                        <ul>
+                            {userStatisticHard.map((number) => (
+                                <li key="{number._id}">{number.word}</li>
+                            ))}
+                        </ul>
+                        <h2>Легкие слова {userStatisticEasy.length}</h2>
+                        <ul>
+                            {userStatisticEasy.map((number) => (
+                                <li key="{number._id}">{number.word}</li>
+                            ))}
+                        </ul>
+                        <h2>Слова из спринта {userStatisticSprint.length}</h2>
+                        <ul>
+                            {userStatisticSprint.map((number) => (
+                                <li key="{number._id}">{number.word}</li>
+                            ))}
+                        </ul>
+                    </>
+                )}
                 {!userStatistic && <h1>Статистика недоступна</h1>}
             </Container>
         </section>
--- src/pages/Textbook.js
+++ src/pages/Textbook.js
@@ -1,44 +1,56 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Container from 'react-bootstrap/Container'
-import Col from 'react-bootstrap/esm/Col'
-import Row from 'react-bootstrap/esm/Row'
-import Group from '../components/Book/Group'
-import Pagination from '../components/Book/Pagination'
-import WordsPage from '../components/Book/WordsPage'
-import DifficultButton from '../components/Book/DiffucultButton'
-import GetStorage from '../components/Book/LocalStorage'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/esm/Col';
+import Row from 'react-bootstrap/esm/Row';
+import Group from '../components/Book/Group';
+import Pagination from '../components/Book/Pagination';
+import WordsPage from '../components/Book/WordsPage';
+import DifficultButton from '../components/Book/DiffucultButton';
+import GetStorage from '../components/Book/LocalStorage';
+import {NavLink} from 'react-router-dom'
 
 function Textbook() {
-    const BASE_URL = `https://teamwork-rs.herokuapp.com/words?`
-    const [user, setUser] = GetStorage('userData', '')
+    const BASE_URL = `https://teamwork-rs.herokuapp.com/words?`;
+    const [user, setUser] = GetStorage('userData', '');
     const [value, setValue] = useState(
         sessionStorage.getItem('page') ? JSON.parse(sessionStorage.getItem('page')).value : '0'
-    )
+    );
     const [pageNumber, setPageNumber] = useState(
         sessionStorage.getItem('page') ? JSON.parse(sessionStorage.getItem('page')).pageNumber : 0
-    )
-    const [words, setWords] = useState([])
-    const [loading, setLoading] = useState(false)
+    );
+    const [words, setWords] = useState([]);
+    const [loading, setLoading] = useState(false);
     window.addEventListener('beforeunload', () => {
-        sessionStorage.setItem('page', JSON.stringify({ pageNumber, value }))
-    })
+        sessionStorage.setItem('page', JSON.stringify({ pageNumber, value }));
+    });
 
     useEffect(() => {
         const getList = async () => {
-            setLoading(true)
-            const res = await axios.get(`${BASE_URL}group=${value}&page=${pageNumber}`)
-            setWords(res.data)
-            setLoading(false)
-        }
-        getList()
-    }, [value, pageNumber, BASE_URL])
+            setLoading(true);
+            const res = await axios.get(`${BASE_URL}group=${value}&page=${pageNumber}`);
+            setWords(res.data);
+            setLoading(false);
+        };
+        getList();
+    }, [value, pageNumber, BASE_URL]);
 
     const changePage = ({ selected }) => {
-        setPageNumber(selected)
+        setPageNumber(selected);
+    };
+    const settings = {
+        fromBook: true,
+        page: pageNumber,
+        value,
     }
     return (
         <section className="textbook-main my-4">
+            <NavLink to="/sprint" state={settings}>
+                перейти на игру Спринт
+            </NavLink>
+            <NavLink to="/audiocall" state={settings}>
+                перейти на игру Аудиовызов
+            </NavLink>
             <Container>
                 <Row className="pb-5">
                     <Col>
@@ -69,7 +81,7 @@ function Textbook() {
                 </Row>
             </Container>
         </section>
-    )
+    );
 }
 
-export default Textbook
+export default Textbook;
