09c9f3f30167f49b342b98b6299354941a729ea0
Alla Borisova <alla.borisova27@gmail.com>
2022-09-05 15:22:11 +0500

    chenge buttons leverl and pagination

--- src/components/Book/Pagination.js
+++ src/components/Book/Pagination.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactPaginate from 'react-paginate'
+import React from 'react';
+import ReactPaginate from 'react-paginate';
 
 function Pagination(props) {
-    const { action, current } = props
-    const numberofPages = 30
+    const { action, current } = props;
+    const numberofPages = 30;
     return (
         <div className="group-btn">
-            <ReactPaginate
+            {/* <ReactPaginate
                 forcePage={current}
                 previousLabel="<"
                 nextLabel=">"
@@ -19,8 +19,27 @@ function Pagination(props) {
                 disabledClassName="disabled-btn"
                 activeClassName="active-btn"
                 pageRangeDisplayed={5}
+            /> */}
+            <ReactPaginate
+                containerClassName="pagination"
+                previousLabel="<"
+                nextLabel=">"
+                breakClassName="page-item"
+                forcePage={current}
+                breakLabel={<a className="page-link">...</a>}
+                pageCount={numberofPages}
+                siblingCount="1"
+                onPageChange={action}
+                pageClassName="page-item"
+                previousClassName="page-item"
+                nextClassName="page-item"
+                pageLinkClassName="page-link"
+                previousLinkClassName="page-link"
+                nextLinkClassName="page-link"
+                activeClassName="active"
+                pageRangeDisplayed={3}
             />
         </div>
-    )
+    );
 }
-export default Pagination
+export default Pagination;
